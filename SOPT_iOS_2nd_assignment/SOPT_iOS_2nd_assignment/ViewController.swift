4a26a2b73ad3586dd52bd735f34c08f088c4d76c
Danna Lee <daeunlee0720@gmail.com>
2020-10-22 21:07:10 +0900

    2주차 과제

---
+++ SOPT_iOS_2nd_assignment/SOPT_iOS_2nd_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT_iOS_2nd_assignment
+//
+//  Created by DANNA LEE on 2020/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
